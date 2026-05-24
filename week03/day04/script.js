6c7bb4119b2ed180fb0d12412b00ce598a020ff1
SafiUllah Saleem <safiullahsubadar@gmail.com>
2026-05-24 14:59:46 +0500

    day 04

---
+++ week03/day04/script.js
@@ -0,0 +1,41 @@
+
+
+// var x = 5
+// console.log(x)
+
+
+// function 
+// function sayHello() {
+//   alert("Hello World");
+// }
+
+
+// sayHello() // "Hello World"
+
+
+// function sayHello() {
+//   return "Hello World";
+// }
+
+// let greeting = sayHello();
+// console.log(greeting) // "Hello World"
+
+
+// function sayHello() {
+//   return "week 03 day 04";
+// }
+
+// console.log(sayHello());
+
+// function sayHello() {
+//   return "Hello i am safiullah saleem";
+// }
+
+// document.getElementById("demo").innerHTML = sayHello();
+
+
+function multiply(a, b=4) {
+  return a * b;
+}
+
+multiply(7)
